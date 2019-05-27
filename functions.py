8764588cd303a6445647ed939a0a82cd6c2883da
Dennis <d.incollu@tiscali.it>
2019-05-27 17:10:13 +0200

    modifica key smd10: [v1, v2, n1, n2]

--- functions.py
+++ functions.py
@@ -67,25 +67,25 @@ def verificaSoluzioneAmmissibile(sat, x2, w2, uk2, Pgac, PsGa, K2diS, A2, Gammad
 def inizializzaSMD10(smd10, rotte, nik2ij, ak2ij, x2, w2, s, K2, Gamma):
     # dizionario dei veicoli per ogni cliente
     veicoliDiCliente = {}
-    # lista di clienti (con eventuali duplicati a causa dello split)
+    # lista di tuple: (cliente, veicolo)
     Gmm = []
 
     for k in rotte:
         for c in rotte[k]:
-            Gmm += [c[1]]
+            Gmm += [(c[1], k)]
             if c[1] in veicoliDiCliente:
                 veicoliDiCliente[c[1]] += [k]
             else:
                 veicoliDiCliente[c[1]] = [k]
 
     lenSMD10 = 0
-    for k in rotte:
+    for v1 in rotte:
         # lista dei nodi del veicolo k (satellite + clienti)
-        listNodes = [s]+[c2 for c1,c2 in rotte[k]]
+        listNodes = [s]+[c2 for c1,c2 in rotte[v1]]
         for n1 in listNodes:
-            for n2 in Gmm:
-                # k = veicolo di destinazione
-                # (indico con v il veicolo di partenza di n2)
+            for n2, v2 in Gmm:
+                # v1 = veicolo di destinazione di n1
+                # v2 = veicolo di partenza di n2
                 # n1 = nodo dietro al quale viene spostato n2
                 # n2 = nodo da spostare dietro n1
 
@@ -96,31 +96,41 @@ def inizializzaSMD10(smd10, rotte, nik2ij, ak2ij, x2, w2, s, K2, Gamma):
                 # perche' per esempio in sat=2 la mossa 8, 4, 7 risulta essere un arco dell'attuale soluzione.
                 # Conviene tenere le mosse dell'attuale soluzione e impostare costo=0???
                 # if (n1, n2) not in rotte[k]:
-                #     print("k: {}, n1: {}, n2: {}".format(k, n1, n2))
+                #     print("v1: {}, n1: {}, n2: {}".format(v1, n1, n2))
 
                 # un veicolo non puo' essere spostato dietro se stesso
                 if n2!=n1:
-                    # DA CORREGGERE
-                    smd10[k, n1, n2] = None
-                    # sono da sommare:
-                    # - v, n2-1, n2
-                    # + v, n2-1, n2+1
-                    # - v, n2, n2+1
-                    # - k, n1, n1+1
-                    # + k, n1, n2
-                    # + k, n2, n1+1
-
-                    # smd10[k, n1, n2] = nik2ij[(k, n2, n1)] + ak2ij[(k, n2, n1)]
-
                     lenSMD10 += 1
+
+                    # sono da sommare: ERRORE
+                    # - v2, n2-1, n2
+                    # + v2, n2-1, n2+1
+                    # - v2, n2, n2+1
+                    # - v1, n1, n1+1
+                    # + v1, n1, n2
+                    # + v1, n2, n1+1
+                    # SONO DA MODIFICARE ENTRAMBE LE ROTTE CHE VENGONO MODIFICATE
+
+                    # smd10[v1, v2, n1, n2] = -(nik2ij[(v2, n2, n1)] + ak2ij[(v2, n2, n1)])
+                    # smd10[v1, v2, n1, n2] += (nik2ij[(v2, n2, n1)] + ak2ij[(v2, n2, n1)])
+                    # smd10[v1, v2, n1, n2] -= (nik2ij[(v2, n2, n1)] + ak2ij[(v2, n2, n1)])
+                    # smd10[v1, v2, n1, n2] -= (nik2ij[(v1, n2, n1)] + ak2ij[(v1, n2, n1)])
+                    # smd10[v1, v2, n1, n2] += (nik2ij[(v1, n2, n1)] + ak2ij[(v1, n2, n1)])
+                    # smd10[v1, v2, n1, n2] += (nik2ij[(v1, n2, n1)] + ak2ij[(v1, n2, n1)])
+
+                    # smd10[v1, n1, n2] = nik2ij[(v1, n2, n1)] + ak2ij[(v1, n2, n1)]
+
                 # else:
                 #     if len(veicoliDiCliente[n2])>1:
                 #         other=list(veicoliDiCliente[n2])
-                #         other.remove(k)
+                #         other.remove(v1)
                 #         for k2 in other:
-                #             if k2!=k:
+                #             if k2!=v1:
                 #                 print("doppio")
-                #                 smd10[k, n1, n2] = None
+                #                 smd10[v1, n1, n2] = None
                 #                 x += 1
 
+    # for smd in smd10:
+    #     print(smd)
     print("smd10: {}\n# elementi: {}".format(smd10, lenSMD10))
+    print("lenSMD10: {}\nlen(SMD10): {}".format(lenSMD10, len(smd10)))
