d25bd50c1ec54ced82b59d6a288d1337e1756568
clodia92 <claudia.porcu92@gmail.com>
2019-11-15 10:36:42 +0100

    modifiche Update SMD

--- functions.py
+++ functions.py
@@ -2205,27 +2205,37 @@ def updateSMD(key, smd10, smd11, rotte, nik2ij, ak2ij, x2, x2TMP, w2, w2TMP, num
     precN2, succN2 = trovaPrecSuccList(rotte[key[1]], key[3])
 
     if len(key) == 5:
-        updateSMD10_10(key, smd10, numeroTotPallet, nik2ij, ak2ij, x2, x2TMP, w2, w2TMP, precN2, succN1, succN2)
+        updateSMD10_10(key, smd10, numeroTotPallet, nik2ij, ak2ij, x2, x2TMP, w2, w2TMP, precN1, precN2, succN1, succN2)
         updateSMD10_11(key, smd11, nik2ij, ak2ij, x2, x2TMP, w2, w2TMP, precN2, succN1, succN2)
     if len(key) == 4:
         updateSMD11_10(key, smd10, nik2ij, ak2ij, x2, x2TMP, w2, w2TMP, precN1, precN2, succN1, succN2)
         updateSMD11_11(key, smd11, nik2ij, ak2ij, x2, x2TMP, w2, w2TMP, precN1, precN2, succN1, succN2)
 
-def updateSMD10_10(key, smd10, numeroTotPallet, nik2ij, ak2ij, x2, x2TMP, w2, w2TMP, precN2, succN1, succN2):
+def updateSMD10_10(key, smd10, numeroTotPallet, nik2ij, ak2ij, x2, x2TMP, w2, w2TMP, precN1, precN2, succN1, succN2):
     # estraggo la chiave
     v1 = key[0]
     v2 = key[1]
     n1 = key[2]
     n2 = key[3]
-    p = key[4]
+    numeroPallet = key[4]
 
-    # se vengono spostati tutti i pallet: arco precN2-n2 eliminato
-    if p == numeroTotPallet:
+    # se vengono spostati tutti i pallet: arco precN2-n2 e/o n2-succN2 eliminato/i
+    if p == numeroTotPallet and v1 != v2:
         for mossa, value in smd10.items:
             # n1 = A
             # + (n1 - n2) , - (n1 - succN1)
-            if mossa[2] == n1 and mossa[0] == v1:
-                pass
+            if mossa[2] == n1 and mossa[0] == v1 :
+                # non sono sicura
+                smd10[mossa[0], mossa[1], n1, mossa[3], mossa[4]] += (x2TMP[v1, n2, n1, n2] * ak2ij[v1, n1, n2]) # numeroPallet
+                if succN1[0] != -1:
+                    for gamma in succN1:
+                        smd10[mossa[0], mossa[1], n1, mossa[3], mossa[4]] -= (x2[mossa[0], gamma, n1, succN1[0]] * ak2ij[mossa[0], n1, succN1[0]])
+                        smd10[mossa[0], mossa[1], n1, mossa[3], mossa[4]] += (x2TMP[mossa[0], gamma, n1, succN1[0]] * ak2ij[mossa[0], n1, succN1[0]])
+
+                        smd10[mossa[0], mossa[1], n2, succN1[0], mossa[4]] += (x2TMP[v1, gamma, n2, succN1[0]] * ak2ij[v1, n2, succN1[0]])
+                if precN1[0] != -1:
+                    for gamma in precN1:
+                        smd10[mossa[0], mossa[1], n1, succN1[0], mossa[4]] += (x2TMP[v1, gamma, n2, succN1[0]] * ak2ij[v1, n2, succN1[0]])
 
 
 def updateSMD10_11(key, smd11, nik2ij, ak2ij, x2, x2TMP, w2, w2TMP, precN2, succN1, succN2):
